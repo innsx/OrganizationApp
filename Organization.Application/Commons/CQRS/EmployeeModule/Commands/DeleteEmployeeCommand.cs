5c6c41349eb5e4f7baeba5855d7c03ef8b8ddc75
Nick Xiong <k_xiong2000@hotmail.com>
2026-07-01 16:44:25 -0400

    Add DeleteEmployeeCommand record & DeleteEmployeeCommand class

---
+++ Organization.Application/Commons/CQRS/EmployeeModule/Commands/DeleteEmployeeCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace Organization.Application.Commons.CQRS.EmployeeModule.Commands
+{
+    public record DeleteEmployeeCommand(string Id, bool isDeleteHasAssociations) : IRequest;
+}
---
+++ Organization.Application/Commons/CQRS/EmployeeModule/Commands/DeleteEmployeeCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using MediatR;
+using Organization.Application.Commons.Exceptions;
+using Organization.Application.Commons.Interfaces.Persistance;
+
+namespace Organization.Application.Commons.CQRS.EmployeeModule.Commands
+{
+    public sealed class DeleteEmployeeCommandHandler : IRequestHandler<DeleteEmployeeCommand>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteEmployeeCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task Handle(DeleteEmployeeCommand request, CancellationToken cancellationToken)
+        {
+            var employeeToDelete = await _unitOfWork.Employees.GetByIdAsync(request.Id);
+
+            if (employeeToDelete == null)
+            {
+                //add this line
+                throw new NotFoundException($"The system does not have any Employee with id = {request.Id}");
+            }
+
+            if (request.isDeleteHasAssociations is true)
+            {
+                throw new InvalidOperationException($"The Employee with id = {request.Id} has associated records and cannot be deleted.");
+            }
+            else
+            {
+                // Proceed with deletion
+                bool isDeleteEmployeeHasAssociation = false;
+
+                _unitOfWork.OpenConnectionAndBeginDbTransaction();
+
+                await _unitOfWork.Employees.SoftDeleteAsync(request.Id, isDeleteEmployeeHasAssociation);
+
+                _unitOfWork.CommitDbTransactionDisposeAndCloseConnectionDispose();
+            }
+        }
+    }
+}
